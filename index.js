ad801721d1d6502e291d92eddb6c42feeec3f47f
shahbaz-kamal <shahbazkamal384@gmail.com>
2024-12-24 23:24:18 +0600

    got all queries data

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const port = process.env.PORT || 5000;
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-app.use(cors());
+// cors
+const corsOption = {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+  optionalSUccessStatus: 200,
+};
+app.use(cors(corsOption));
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -13,7 +19,6 @@ app.get("/", (req, res) => {
 });
 
 // mongodb connection
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jxshq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -35,9 +40,27 @@ async function run() {
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+    // *declaring collections
+    const queryCollections = client
+      .db("suggestify-db")
+      .collection("query-collection");
+    // *getting all query
+
+    app.get("/all-query", async (req, res) => {
+      const result = await queryCollections.find().toArray();
+      res.send(result);
+    });
+
+    //*adding query to db
+
+    app.post("/add-query", async (req, res) => {
+      const newQuery = req.body;
+      const result = await queryCollections.insertOne(newQuery);
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
